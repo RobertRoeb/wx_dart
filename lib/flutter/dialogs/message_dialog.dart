27994bd4a4d42fe7df420e0153fc70bdea83f6ea
Robert Roebling <robert.roebling@gmail.com>
2026-03-30 20:04:17 +0200

    Docs for  wxMessageDialog, corrected setExtendedMessage
      - Move extended message below main message
      - Make main message bigger if there is an extended message

--- lib/flutter/dialogs/message_dialog.dart
+++ lib/flutter/dialogs/message_dialog.dart
@@ -9,6 +9,36 @@ part of '../../wx_dart.dart';
 
 // ------------------------- wxMessageDialog ----------------------
 
+/// Standard dialog to show a message to the user.
+/// 
+/// Styles can be used to add icon indicating the nature of the message
+/// (such as information, warning or error) and which buttons to display
+/// 
+///```dart
+///   final msg = WxMessageDialog( this, "Pressed OK", caption: "wxDart" );
+///   msg.setExtendedMessage( "File: $data" );
+///   msg.showModal(null);
+///```
+///
+/// # Style constants
+/// | constant | meaning |
+/// | -------- | -------- |
+/// | wxICON_ERROR | indicates an error |
+/// | wxICON_WARNING | indicates a warning |
+/// | wxICON_QUESTION | question (mark) |
+/// | wxICON_INFORMTION | information |
+/// | wxICON_HAND | shows a hand |
+/// | wxICON_AUTH_NEEDED | authentication is needed |
+/// | wxYES | button with id wxID_YES |
+/// | wxNO | button with id wxID_NO |
+/// | wxYES_NO | 2 button with id wxID_NO and wxID_YES |
+/// | wxOK | button with id wxID_OK |
+/// | wxCANCEL | button with id wxID_CANCEL |
+/// | wxAPPLY | button with if wxID_APPLY |
+/// | wxHELP | button with if wxID_HELP |
+/// | wxSTAY_ON_TOP | always stays on top |
+/// | wxCENTRE | centre above owning frame |
+
 class WxMessageDialog extends WxDialog {
   WxMessageDialog( WxWindow? parent, String message, { String caption = '', int style = wxOK|wxCENTRE, WxPoint pos = wxDefaultPosition } ) 
   : super( parent, -1, caption, pos: pos )
@@ -57,38 +87,47 @@ class WxMessageDialog extends WxDialog {
   String _help = "Help";
   late WxBoxSizer _mainSizer;
 
+  /// Sets the message of the message box
   void setMessage( String message ) {
     _message.setLabel( message );
     layout();  // does nothing on wxDart Flutter
   }
 
+  /// Sets the extended (extra) message of the message box
   void setExtendedMessage( String message ) {
     if (_extendedMessage != null) {
       _extendedMessage!.setLabel( message );
     } else {
       int count = _mainSizer.getItemCount();
       _extendedMessage = WxStaticText(this, -1, message );
-      _mainSizer.insert( count-2, _extendedMessage!, flag: wxALL, border: 10 );
+      _mainSizer.insert( count-1, _extendedMessage!, flag: wxALL, border: 10 );
+
+      final pointSize = wxNORMAL_FONT.getPointSize();
+      _message.setFont( WxFont( pointSize*1.3 ) );
     }
     layout();  // does nothing on wxDart Flutter
   }
 
+  /// Sets the label of the button with the wxID_OK ID
   void setOKLabel( String label ) {
     _ok = label;
     layout();  // does nothing on wxDart Flutter
   }
 
+  /// Sets the label of the button with the wxID_HELP ID
   void setHelpLabel( String label ) {
     _help = label;
     layout();  // does nothing on wxDart Flutter
   }
 
+  /// Sets the label of the yes/no buttons
   void setYesNoLabels( String yes, String no ) {
     _yes = yes;
     _no = no;
     layout();  // does nothing on wxDart Flutter
   }
 
+  /// Sets the label of the yes/no/cancel buttons
   void setYesNoCancelLabels( String yes, String no, String cancel ) {
     _yes = yes;
     _no = no;
