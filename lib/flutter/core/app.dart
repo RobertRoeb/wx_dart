f762604b4fe1aad50c10288b38712e8da92a9bb4
Robert Roebling <robert.roebling@gmail.com>
2026-08-06 08:34:36 +0200

    Synthesize idle event after window resize (idle_detector does not do that)

--- lib/flutter/core/app.dart
+++ lib/flutter/core/app.dart
@@ -113,6 +113,9 @@ class WxDartAppWidgetState extends State<WxDartAppWidget>
   late final AppLifecycleListener _listener;
   double _screenWidth = 1280;
   double _screenHeight = 800;
+  double _oldScreenWidth = 1280;
+  double _oldScreenHeight = 800;
+  bool _idleScheduled = false;
 
   @override
   void initState()
@@ -183,6 +186,19 @@ class WxDartAppWidgetState extends State<WxDartAppWidget>
   {
     _screenWidth = MediaQuery.sizeOf(context).width;
     _screenHeight = MediaQuery.sizeOf(context).height;
+    if ((_screenWidth != _oldScreenWidth) || (_screenHeight != _oldScreenHeight)) {
+      _oldScreenHeight = _screenHeight;
+      _oldScreenWidth = _screenWidth;
+      if (!_idleScheduled) {
+        _idleScheduled = true;
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          for (final tlw in _topLevelWindows) {
+            tlw._callRecursiveOnInternalIdle( tlw );
+          }
+          _idleScheduled = false;
+        });        
+      }
+    }
 
     if (theTLW == null) {
       return Text( "There is no WxFrame for WxApp to show!" );
